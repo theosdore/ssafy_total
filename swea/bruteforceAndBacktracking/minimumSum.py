ac09f88a125fa5ef824255f9fe5c0a78a0103463
theosdore <shine379@naver.com>
2025-09-24 17:59:16 +0900

    aa

---
+++ swea/bruteforceAndBacktracking/minimumSum.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("C:\\Programming\\SSAFY_TOTAL\\swea\\bruteforceAndBacktracking\\input.txt")
+
+def recur():
+    #왼쪽 스택
+    #오른쪽 스택
+    #visited로 구현하기
+    #최소값보다 이미 커지면 return
+
+for t in range(1, int(input())+1):
+    N = int(input())
