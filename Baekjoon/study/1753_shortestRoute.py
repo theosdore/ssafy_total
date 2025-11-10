d41e2c1fb0198fcb83cd9935528163a3b1934828
theosdore <shine379@naver.com>
2025-11-10 17:57:53 +0900

    스터디

--- Baekjoon/study/1753_shortestRoute.py
+++ Baekjoon/study/1753_shortestRoute.py
@@ -1,7 +1,10 @@
 # v : 정점 개수  e : 간선 개수
-v, e = map(int, input().split())
+V, E = map(int, input().split())
 # k : 시작 정점 번호
-k = int(input())
+K = int(input())
+# 인접 리스트
+table = [[] for _ in range(E+1)]
 # 간선 정보 u,v,w
-arr = [ tuple(map(int, input().split())) for _ in range(e)]
-
+for _ in range(E):
+    u,v,w = map(int(input().split()))
+    table[u].append((v,w))
